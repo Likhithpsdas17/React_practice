aa6ceaefaa587e91dbe68ebd322c5849eff1559f
Likhithpsdas17 <likhithdaz16@gmail.com>
2026-04-08 20:49:19 +0530

    Tab updated

--- React-7/react_07/src/components/Tab.jsx
+++ React-7/react_07/src/components/Tab.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 function Tab() {
-    const [isHero, setIsHero] = useState(false);
+    const [cartoon,setCortoon] = useState([]);
 
     function handleHeros() {
-        setIsHero(true);
+        setCortoon(characters.heroes)
     }
 
     function handleVillian() {
-        setIsHero(false);
+        setCortoon(characters.villains)
     }
 
     const characters = {
@@ -32,14 +32,66 @@ function Tab() {
             <h1>Tab</h1>
             <button onClick={handleHeros}>Heroes</button>
             <button onClick={handleVillian}>Villains</button>
+            {
+                cartoon.map((item)=>{
+                return(
+                    <div>
+                        <h1>Name: {item.name}</h1>
+                    </div>
+                )
+            })
+        
+            }
 
-            <ol>
-                {(isHero ? characters.heroes : characters.villains).map(({ name }) => (
-                    <li key={name}>{name}</li>
-                ))}
-            </ol>
-        </div>
+         </div>
     );
 }
 
 export default Tab;
+
+
+// import { useState } from "react";
+
+// function Tab() {
+//     const [isHero, setIsHero] = useState(false);
+
+//     function handleHeros() {
+//         setIsHero(true);
+//     }
+
+//     function handleVillian() {
+//         setIsHero(false);
+//     }
+
+//     const characters = {
+//         heroes: [
+//             { name: "IRON MAN" },
+//             { name: "ANT MAN" },
+//             { name: "Spider-man" },
+//             { name: "Bat man" },
+//             { name: "Super-man" },
+//         ],
+//         villains: [
+//             { name: "Thanos" },
+//             { name: "Venom" },
+//             { name: "Kang the conqueror" },
+//             { name: "Joker" },
+//         ],
+//     };
+
+//     return (
+//         <div>
+//             <h1>Tab</h1>
+//             <button onClick={handleHeros}>Heroes</button>
+//             <button onClick={handleVillian}>Villains</button>
+
+//             <ol>
+//                 {(isHero ? characters.heroes : characters.villains).map(({ name }) => (
+//                     <li key={name}>{name}</li>
+//                 ))}
+//             </ol>
+//         </div>
+//     );
+// }
+
+// export default Tab;
